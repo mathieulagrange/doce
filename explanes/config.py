039f6d75a6af4f1db083e0d1eef3ef9b1ee121eb
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2020-06-01 12:16:47 +0200

    handle aternative setting selection

--- explanes/config.py
+++ explanes/config.py
@@ -1,6 +1,8 @@
 import smtplib
 import types
 import inspect
+import os
+import sys
 
 class Config():
     project = types.SimpleNamespace()
@@ -13,6 +15,13 @@ class Config():
     path.processing = ''
     path.storage = ''
     path.output = ''
+
+    def makePaths(self):
+        for sns in self.__getattribute__('path').__dict__.keys():
+            path = self.__getattribute__('path').__getattribute__(sns)
+            if not os.path.exists(path):
+                print(path)
+                # os.makedirs(path)
 
     def __str__(self):
         cString = ''
@@ -41,4 +50,38 @@ class Config():
         server.starttls()
         server.login('expcode.mailer@gmail.com', 'tagsqtlirkznoxro')
         server.sendmail("expcode.mailer@gmail.com", self.project.address, header+msg+self.toHtml())
-        server.quit()
+        server.quit
+
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
