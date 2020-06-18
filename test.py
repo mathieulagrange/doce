27f86990db438448c2224a64af3e8b8f196e3467
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2020-06-18 13:29:11 +0200

    try to fix ordering issue with python3.5

--- test.py
+++ test.py
@@ -1,55 +1,19 @@
-from collections import OrderedDict
-from types import FunctionType
 
 
-class StaticOrderHelper(type):
-    # Requires python3.
-    def __prepare__(name, bases, **kwargs):
-        return OrderedDict()
 
-    def __new__(mcls, name, bases, namespace, **kwargs):
-        namespace['_field_order'] = [
-                k
-                for k, v in namespace.items()
-                if not k.startswith('__') and not k.endswith('__')
-                    and not isinstance(v, (FunctionType, classmethod, staticmethod))
-        ]
-        return type.__new__(mcls, name, bases, namespace, **kwargs)
+import explanes as exp
+import numpy as np
 
+factors = exp.Factors()
 
-class Person(metaclass=StaticOrderHelper):
-    first_name = 'First Name'
-    last_name = 'Last Name'
-    phone_number = '000-000'
+factors.task = ['id', 'time', 'vec', 'idBaseline', 'timeBaseline', 'vecBaseline']
+factors.computeType = ['none', 'train', 'test']
+factors.doing = 10*np.arange(10)
+# factors.textureSize = 200
+# factors.embeddingSize = 128
+# factors.batchSize = 2
 
-    @classmethod
-    def classmethods_not_included(self):
-        pass
-
-    @staticmethod
-    def staticmethods_not_included(self):
-        pass
-
-    def methods_not_included(self):
-        pass
-
-
-print(Person._field_order)
-
-
-# import explanes as exp
-# import numpy as np
-#
-# factors = exp.Factors()
-#
-# factors.task = ['id', 'time', 'vec', 'idBaseline', 'timeBaseline', 'vecBaseline']
-# factors.computeType = ['none', 'train', 'test']
-# factors.doing = 10*np.arange(10)
-# # factors.textureSize = 200
-# # factors.embeddingSize = 128
-# # factors.batchSize = 2
-#
-# print(factors.getFactorNames())
+print(factors.getFactorNames())
 
 #
 # for f in factors.settings():
