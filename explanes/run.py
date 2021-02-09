c54f474e8a0b9001b2153fde4a615bae50750e06
Mathieu Lagrange <mathieu.lagrange@ls2n.fr>
2021-02-09 14:53:43 +0100

    fix

--- explanes/run.py
+++ explanes/run.py
@@ -249,6 +249,7 @@ def dataFrameDisplay(experiment, args, config, selectDisplay, selectFactor):
 
   mask = experiment.mask
   if selectFactor:
+    # print(experiment.factor.factors())
     fi = experiment.factor.factors().index(selectFactor)
     mask = el.util.expandMask(mask, selectFactor, experiment.factor)
     ma=copy.deepcopy(mask)
